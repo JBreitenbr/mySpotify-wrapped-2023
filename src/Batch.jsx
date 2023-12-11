74e1fed2d3b553c6faadd937b91d3ea993b961e2
JBreitenbr <JuliaBreitenbruch@gmail.com>
2023-12-11 11:57:52 +0000

    added tooltip

--- src/Batch.jsx
+++ src/Batch.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import {Tooltip} from 'react-tooltip';
 import {tops} from './spotiData.js';
 export default function Batch(props) {
   let sub=tops.slice(props.num*10-10,props.num*10);
@@ -6,13 +7,30 @@ export default function Batch(props) {
   const topItems = sub.map(
 (item,index) => {
 return (
-  <li key={index} className="track-item"  ><div className="list-wrapper">
+ <><li key={index} className="track-item"  ><a data-tooltip-id={`tooltip_${index}`} key={index}><div className="list-wrapper">
     <div className="track-number" style={index<9&&(props.num==1||props.num==10)?{marginLeft:"12px"}:{marginLeft:"4px"}}>{index+`${props.num*10-10}`*1+1}</div>
     <img className="track-album" src={item.pic} />
     <div className="track-info">
     <div className="track-name" style={index==6&&props.num==2||index==3&&props.num==5||index==1&&props.num==9?{fontSize:"12px"}:{fontSize:"14px"}}>{item.track}</div>
      <div className="track-artist">{item.artist}</div></div></div> 
-  </li>
+  <Tooltip id={`tooltip_${index}`} key={index}>
+  <div className="tooltip">
+    <ul>
+      <li>Popularity on Spotify: {item.popularity}</li>
+      <li>Released in: {item.release_year}</li>
+      <li>Danceability: {item.danceability}</li>
+      <li>Energy: {item.energy}</li>
+      <li>Loudness: {item.loudness}</li><li>Speechiness: {item.speechiness}</li>
+      <li>Acousticness: {item.acousticness}</li>
+      <li>Instrumentalness: {item.instrumentalness}</li>
+      <li>Liveness: {item.liveness}</li>
+      <li>Valence: {item.valence}</li>
+      <li>Tempo: {item.tempo}</li>
+      <li>Duration (ms): {item.duration_ms}</li>
+
+    </ul></div></Tooltip></a></li>
+
+</>
             )
         });
 
--- src/Batch_md.jsx
+++ src/Batch_md.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import {Tooltip} from 'react-tooltip';
 import {tops} from './spotiData.js';
 export default function Batch_md(props) {
   let top1=tops.slice(props.num*20-20,props.num*20-10);
@@ -6,7 +7,7 @@ export default function Batch_md(props) {
   const topItems1 = top1.map(
 (item,index) => {
 return (
-  <li key={index} className="track-item"  ><div className="list-wrapper">
+  <li key={index} className="track-item"  ><a data-tooltip-id={`tooltip1_${index}`} key={index}><div className="list-wrapper">
     <div className="track-number"
       style={index<9&&props.num==1?{marginLeft:"18px",fontSize:"1.15em"}:{marginLeft:"10px",fontSize:"1.15em"}}
       >{index+`${props.num*20-20}`*1+1}</div>
@@ -14,19 +15,49 @@ return (
     <div className="track-info">
     <div className="track-name" style={index==3&&props.num==3||index==1&&props.num==5?{fontSize:"0.9em"}:{fontSize:"1.1em"}}
       >{item.track}</div>
-     <div className="track-artist">{item.artist}</div></div></div> 
+     <div className="track-artist">{item.artist}</div></div></div> <Tooltip id={`tooltip1_${index}`} key={index}>
+  <div className="tooltip">
+    <ul>
+      <li>Popularity on Spotify: {item.popularity}</li>
+      <li>Released in: {item.release_year}</li>
+      <li>Danceability: {item.danceability}</li>
+      <li>Energy: {item.energy}</li>
+      <li>Loudness: {item.loudness}</li><li>Speechiness: {item.speechiness}</li>
+      <li>Acousticness: {item.acousticness}</li>
+      <li>Instrumentalness: {item.instrumentalness}</li>
+      <li>Liveness: {item.liveness}</li>
+      <li>Valence: {item.valence}</li>
+      <li>Tempo: {item.tempo}</li>
+      <li>Duration (ms): {item.duration_ms}</li>
+
+    </ul></div></Tooltip></a>
   </li>
             )
         });
   const topItems2 = top2.map(
 (item,index) => {
 return (
-  <li key={index} className="track-item"  ><div className="list-wrapper">
+  <li key={index} className="track-item"  ><a data-tooltip-id={`tooltip2_${index}`} key={index}><div className="list-wrapper">
     <div className="track-number" style={index<9&&props.num==5?{marginLeft:"15px","fontSize":"1.15em"}:{marginLeft:"7px","fontSize":"1.15em"}}>{index+`${props.num*20-10}`*1+1 }</div>
     <img className="track-album" src={item.pic} />
     <div className="track-info">
     <div className="track-name" style={index==6&&props.num==1||index==6&&props.num==5?{fontSize:"0.9em"}:{fontSize:"1.1em"}} >{item.track}</div>
-     <div className="track-artist">{item.artist}</div></div></div> 
+     <div className="track-artist">{item.artist}</div></div></div>   <Tooltip id={`tooltip2_${index}`} key={index}>
+  <div className="tooltip">
+    <ul>
+      <li>Popularity on Spotify: {item.popularity}</li>
+      <li>Released in: {item.release_year}</li>
+      <li>Danceability: {item.danceability}</li>
+      <li>Energy: {item.energy}</li>
+      <li>Loudness: {item.loudness}</li><li>Speechiness: {item.speechiness}</li>
+      <li>Acousticness: {item.acousticness}</li>
+      <li>Instrumentalness: {item.instrumentalness}</li>
+      <li>Liveness: {item.liveness}</li>
+      <li>Valence: {item.valence}</li>
+      <li>Tempo: {item.tempo}</li>
+      <li>Duration (ms): {item.duration_ms}</li>
+
+    </ul></div></Tooltip></a>
   </li>
             )
         });
